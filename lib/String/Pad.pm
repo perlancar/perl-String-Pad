13c2da3517220ff4f289ec873ddad3efa2bcd189
perlancar (@pc-office) <perlancar@gmail.com>
2014-12-10 17:59:24 +0700

    Initial commit

---
+++ lib/String/Pad.pm
@@ -0,0 +1,56 @@
+package String::Pad;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(
+                       pad
+               );
+
+sub pad {
+    my ($text, $width, $which, $padchar, $is_trunc) = @_;
+    if ($which) {
+        $which = substr($which, 0, 1);
+    } else {
+        $which = "r";
+    }
+    $padchar //= " ";
+
+    my $w = length($text);
+    if ($is_trunc && $w > $width) {
+        $text = substr($text, 0, $width, 1);
+    } else {
+        if ($which eq 'l') {
+            $text = ($padchar x ($width-$w)) . $text;
+        } elsif ($which eq 'c') {
+            my $n = int(($width-$w)/2);
+            $text = ($padchar x $n) . $text . ($padchar x ($width-$w-$n));
+        } else {
+            $text .= ($padchar x ($width-$w));
+        }
+    }
+    $text;
+}
+
+1;
+# ABSTRACT: String padding routines
+
+=head1 FUNCTIONS
+
+=head2 pad($text, $width[, $which[, $padchar[, $truncate]]]) => STR
+
+Return C<$text> padded with C<$padchar> to C<$width> columns. C<$which> is
+either "r" or "right" for padding on the right (the default if not specified),
+"l" or "left" for padding on the right, or "c" or "center" or "centre" for
+left+right padding to center the text.
+
+C<$padchar> is whitespace if not specified. It should be string having the width
+of 1 column.
+
+=cut
